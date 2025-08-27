24f1524a40676ab142706ce5fb581fe3716d1309
ab-jubair <jubair7624@gmail.com>
2025-08-27 15:28:30 +0600

    modified the button and card section

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ console.log("js is conneceted");
 const loadCategories = async () => {
   try {
     const res = await fetch(
-      "https://openapi.programming-hero.com/api/peddy/categories");
+      "https://openapi.programming-hero.com/api/peddy/categories"
+    );
     const data = await res.json();
     displayCategories(data.categories);
   } catch (error) {
@@ -15,7 +16,9 @@ const loadCategories = async () => {
 // load api for pet card
 const loadPatDetails = async () => {
   try {
-    const res = await fetch("https://openapi.programming-hero.com/api/peddy/pets");
+    const res = await fetch(
+      "https://openapi.programming-hero.com/api/peddy/pets"
+    );
     const data = await res.json();
     displayPetDetais(data.pets);
   } catch (error) {
@@ -25,9 +28,11 @@ const loadPatDetails = async () => {
 //  pet API by id
 const petsById = async (id) => {
   try {
-    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${id}`);
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/peddy/pet/${id}`
+    );
     const data = await res.json();
- console.log(data.petData);
+    console.log(data.petData);
   } catch (error) {
     console.error("Somthing Wrong in API for pets API by Id", error);
   }
@@ -35,43 +40,45 @@ const petsById = async (id) => {
 //  pets API by Pets (dog , bird, cat)
 const petsCategories = async (category) => {
   try {
-    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`);
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/peddy/category/${category}`
+    );
     const data = await res.json();
- displayPetDetais(data.data);
+    displayPetDetais(data.data);
   } catch (error) {
     console.error("Somthing Wrong in API for pets API by Id", error);
   }
 };
 
 // for click the details button
-const btnclick =(id) =>{
-    petsCategories(id)
-}
+const btnclick = (id) => {
+  petsCategories(id);
+};
 
 // display categories
 const displayCategories = (data) => {
   const CatBtnContainer = document.getElementById("categoriesBtn");
   data.forEach((item) => {
     const CatBtn = document.createElement("div");
-    CatBtn.innerHTML = `<button onclick="btnclick('${item.category}')" class="btn px-14 py-2 h-14"><img class="h-full " src="${item.category_icon}" alt=""><p class ="text-xl">${item.category} </p></button>`;   
-    CatBtnContainer.append(CatBtn);    
+    CatBtn.innerHTML = `<button onclick="btnclick('${item.category}')" class="btn lg:px-14 md:px-8 py-2 h-14"><img class="h-full " src="${item.category_icon}" alt=""><p class ="text-xl">${item.category} </p></button>`;
+    CatBtnContainer.append(CatBtn);
   });
 };
 
 //  display All pets
 const displayPetDetais = (data) => {
-  const petscontainer = document.getElementById('PetsDisplay')
-  petscontainer.innerHTML = ""
+  const petscontainer = document.getElementById("PetsDisplay");
+  petscontainer.innerHTML = "";
   data.forEach((item) => {
-      const petCard = document.createElement('div')
-      petCard.innerHTML = `
-       <div class="card p-1 mb-4 border shadow-sm">
-                    <figure>
-                        <img class ="w-80 h-60 rounded-xl mt-5" src="${item.image}"
+    const petCard = document.createElement("div");
+    petCard.innerHTML = `
+       <div class="card p-1 pt-4 mb-4 border shadow-sm">
+                    <figure class="px-4">
+                        <img class ="w-auto h-60 object-cover rounded-xl mx-4" src="${item.image}"
                             alt="Shoes" />
                     </figure>
-                    <div class="card-body rounded-xl ">
-                        <h2 class="card-title">${item.category}</h2>
+                    <div class=" px-4 rounded-xl ">
+                        <h2 class="card-title py-2 pl-2">${item.category}</h2>
                         <div class="gray-font flex flex-col gap-1">
                             <div class="flex items-center gap-1"><img class="h-7"
                                     src="https://img.icons8.com/dotty/80/hachiko.png" alt="hachiko" />
@@ -89,20 +96,18 @@ const displayPetDetais = (data) => {
                                 <pclass="text-start textx">Price: ${item.price}$</p>
                             </div>
                         </div>
-                        <div class="flex justify-between border-t pt-1.5 ">
+                    </div>
+                        <div class="flex justify-between border-t pt-1.5 p-2 ">
                             <button class="btn "><img class="w-6"
                                     src="https://img.icons8.com/stickers/100/facebook-like-skin-type-1.png"
                                     alt="facebook-like-skin-type-1" /></button>
                             <button class="btn btnColor"> Adopt</button>
                             <button class="btn btnColor">Details</button>
                         </div>
-                    </div>
                 </div>
-      `
-      petscontainer.append(petCard)
-    
-  })
-  
+      `;
+    petscontainer.append(petCard);
+  });
 };
 
 loadCategories();
